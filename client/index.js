af5e4268b34d6cc982d1bebf649520b8cf20ff51
Ionică Bizău <bizauionica@yahoo.com>
2015-04-07 18:56:54 +0300

    Added the event value.

--- client/index.js
+++ client/index.js
@@ -6,6 +6,6 @@ exports.init = function () {
 exports.req = function (ev, req) {
     var self = this;
     self.link("request", function (err, response) {
-        self.emit("response", { err: err, res: response });
+        self.emit("response", null, { err: err, res: response });
     }).send(null, req);
 };
