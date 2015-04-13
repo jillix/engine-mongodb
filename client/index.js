bac75b8a00f45958341c82d9c6d6787845fa7fdc
Ionică Bizău <bizauionica@yahoo.com>
2015-04-13 15:52:08 +0300

    Action related events

--- client/index.js
+++ client/index.js
@@ -6,6 +6,11 @@ exports.init = function () {
 exports.req = function (ev, req) {
     var self = this;
     self.link("request", function (err, response) {
+        if (err) {
+            self.emit("res_error_" + req.action, { err: err });
+        } else {
+            self.emit("res_success_" + req.action, response);
+        }
         self.emit("response", null, { err: err, res: response });
     }).send(null, req);
 };
